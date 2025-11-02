50c53d5cba0f9edd0c424898ce027b0ce5e08dd8
liuwenqiang1314 <2018305346@qq.com>
2025-11-02 09:35:17 +0800

    sse支持

--- comon/src/main/java/org/im/eventbus/Topic.java
+++ comon/src/main/java/org/im/eventbus/Topic.java
@@ -4,4 +4,10 @@ public class Topic {
 
     // 温度数据发送事件总线的地址，其他verticle可以订阅该地址来接收温度数据
     public final static String SENSOR_UPDATE_ADDRESS = "sensor.update.address";
+
+    // 平均温度数据发送事件总线的地址，其他verticle可以订阅该地址来接收温度数据
+    public final static String SENSOR_AVERAGE_ADDRESS = "sensor.average.address";
+
+
+
 }
